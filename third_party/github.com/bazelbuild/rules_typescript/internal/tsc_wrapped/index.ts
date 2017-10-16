c8527c51d086ccdb19d71e10dc9b4aa6e2b5f178
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2017-10-16 14:05:28 -0400

    Add test for previous commit.

--- third_party/github.com/bazelbuild/rules_typescript/internal/tsc_wrapped/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './tsconfig';
-export * from './compiler_host';
-export * from './file_cache';
-export * from './worker';
-export * from './manifest';
-export * from './umd_module_declaration_transform';
