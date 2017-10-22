069c4a6d63171daadfdbca565b1028110fd06a67
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2017-10-22 16:47:11 -0400

    Update README.md

---
+++ third_party/github.com/bazelbuild/rules_typescript/internal/tsc_wrapped/index.ts
@@ -0,0 +1,6 @@
+export * from './tsconfig';
+export * from './compiler_host';
+export * from './file_cache';
+export * from './worker';
+export * from './manifest';
+export * from './umd_module_declaration_transform';
