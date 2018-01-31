a7664e153a15c499d6f66a0ec10ad2d1188cf20a
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2018-01-31 07:07:45 -0800

    Make it more convenient to get a perf trace for TypeScript compilation.

--- internal/e2e/rollup/bundle-min_golden.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";console.log("Hello, Alice in Wonderland");var n=function(){function n(){}return n.prototype.a=function(){return document.a},n}();console.error((new n).a())}();
