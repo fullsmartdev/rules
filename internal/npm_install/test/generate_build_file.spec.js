7602af166581ecf7b8530b09dd80f12566b0d810
HideAKi <hideaki.chain@gmail.com>
2018-09-14 12:04:19 -0700

    Wrap proto.Unmarshal() errors in a custom error to enable callers to identify such errors.

--- internal/npm_install/test/generate_build_file.spec.js
+++
@@ -1,7 +0,0 @@
-const check = require('./check');
-
-describe('build file generator', () => {
-  it('should produce a BUILD file from the node_modules file structure', () => {
-    check();
-  });
-});
