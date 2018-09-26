7c5b6c3e01966215c2b3f8b7073f0fd3d64cefc0
HideAKi <hideaki.chain@gmail.com>
2018-09-26 14:54:29 -0700

    Fix optionalDependencies issue with npm_install (#341)

--- third_party/github.com/bazelbuild/rules_typescript/internal/e2e/package_typescript_2.9/main.spec.ts
+++
@@ -1,7 +0,0 @@
-import * as main from './main';
-
-describe('main', () => {
-  it('should compile and run with @bazel/typescript npm package', () => {
-    expect(main.test()).toEqual('test');
-  });
-});
--- third_party/github.com/bazelbuild/rules_typescript/internal/e2e/package_typescript_2.9/main.ts
+++
@@ -1,8 +0,0 @@
-import * as os from 'os';
-
-console.log('Platform: ' + os.platform());
-console.log('Architecture: ' + os.arch());
-
-export function test() {
-  return 'test';
-}
