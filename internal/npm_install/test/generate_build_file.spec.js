f959b0175e212ce2c99dcfe757ea13644043954e
HideAKi <hideaki.chain@gmail.com>
2018-10-04 16:21:43 -0700

    Update docs for release

--- internal/npm_install/test/generate_build_file.spec.js
+++
@@ -1,9 +0,0 @@
-const {check, files} = require('./check');
-
-describe('build file generator', () => {
-  files.forEach(file => {
-    it(`should produce a BUILD file for ${file}`, () => {
-      check(file);
-    });
-  });
-});
