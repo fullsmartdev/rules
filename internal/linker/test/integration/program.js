26ab8d6b33f0e944b69a9aadf95ddab312f1656f
HideAKi <hideaki.chain@gmail.com>
2019-08-30 17:54:16 -0700

    Add more detailed logging of latency for requests with no TypeScript.

--- internal/linker/test/integration/program.js
+++
@@ -1,6 +0,0 @@
-// First-party package from ./pkg_a
-const a = require('a');
-// Third-party package installed in the root node_modules
-const semver = require('semver');
-
-console.log(a.addA(semver.clean(' =v1.2.3 ')));
