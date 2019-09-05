e6cb8a32754a51f0b222bd00d0616487a1fa344f
HideAKi <hideaki.chain@gmail.com>
2019-09-05 16:19:59 -0700

    Internal change.

--- packages/rollup/test/code_splitting/spec.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-describe('rollup code splitting', () => {
-  it('should produce a chunk for lazy loaded code', () => {
-    expect(fs.existsSync(require.resolve(__dirname + '/bundle/bundle.js'))).toBeTruthy();
-    expect(fs.existsSync(require.resolve(__dirname + '/bundle/chunk.js'))).toBeTruthy();
-  });
-});
--- packages/rollup/test/multiple_entry_points/spec.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-describe('rollup multiple entry points', () => {
-  it('should produce a chunk for each entry point', () => {
-    expect(fs.existsSync(require.resolve(__dirname + '/chunks/one.js'))).toBeTruthy();
-    expect(fs.existsSync(require.resolve(__dirname + '/chunks/two.js'))).toBeTruthy();
-  });
-});
