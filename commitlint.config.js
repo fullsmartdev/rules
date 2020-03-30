8da00e58361362e8602b14dbb61a72cf824843ad
HideAKi <hideaki.chain@gmail.com>
2020-03-30 16:52:33 -0700

    feat(typescript): wire up use_angular_plugin attribute

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'scope-enum': [
+      2, 'always',
+      [
+        'builtin',
+        'create',
+        'examples',
+        'hide-bazel-files',
+        'jasmine',
+        'karma',
+        'labs',
+        'protractor',
+        'rollup',
+        'terser',
+        'typescript',
+        'worker',
+        'docs',
+      ]
+    ]
+  }
+}
