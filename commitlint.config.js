8032713ac3ca940c5547de71b9cc65c86a26dc5f
HideAKi <hideaki.chain@gmail.com>
2020-05-07 07:39:44 -0700

    ci: fix broken master (#1875)

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 120],
+    'scope-enum': [
+      2, 'always',
+      [
+        'builtin',
+        'create',
+        'examples',
+        'jasmine',
+        'karma',
+        'labs',
+        'protractor',
+        'rollup',
+        'terser',
+        'typescript',
+        'worker',
+        'docs',
+      ]
+    ]
+  }
+}
