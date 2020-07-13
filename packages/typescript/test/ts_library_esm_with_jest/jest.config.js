d309d32289dca4682ddaa116eb0b0b24497cdc64
HideAKi <hideaki.chain@gmail.com>
2020-07-13 10:16:32 -0700

    Purely Google refactor

--- packages/typescript/test/ts_library_esm_with_jest/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {'^.+\\.mjs?$': ['babel-jest', {configFile: __dirname + '/.babelrc'}]},
-  testMatch: ['**/?(*.)(spec|test).?(m)js?(x)'],
-  moduleFileExtensions: ['js', 'mjs'],
-};
